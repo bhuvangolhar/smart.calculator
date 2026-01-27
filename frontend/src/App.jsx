d2bc178e6b2b20112cd599f6b5a21278afe1d452
Bhuvan Golhar <bhuvangolhar2025@gmail.com>
2026-01-27 23:39:53 +0530

    created a simple webpage for our Smart Calculator app

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <div className="app">
+      <h1>Smart Calculator 🧮</h1>
+      <p>A very basic calculator app (features coming soon).</p>
+
+      <div className="calculator-box">
+        Calculator UI will be added here
+      </div>
+
+      <button>Start Calculating</button>
+    </div>
+  );
+}
+
+export default App;
